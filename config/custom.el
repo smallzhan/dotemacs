0883af63069f949be07bffee3d15f40b17a074fe
Guoqiang Jin <ustczhan@gmail.com>
2014-05-20 17:31:05 +0800

    增加　global-auto-revert-mode

--- config/custom.el
+++ config/custom.el
@@ -1,7 +1,7 @@
 ;;=========================================================
 ;;============关于 custom 的配置=============================
 ;;=========================================================
-;;Time-stamp: <smallqiang 2014-05-19 13:56:07>
+;;Time-stamp: <smallqiang 2014-05-20 15:01:57>
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -12,6 +12,9 @@
  '(custom-enabled-themes (quote (tangotango)))
  '(custom-safe-themes (quote ("c57df0604a534991aed8e3240cc28b22c678d8e575a218f2d8eae8428743c869" default)))
  '(menu-bar-mode nil)
+ '(safe-local-variable-values (quote ((python-shell-completion-string-code . "';'.join(get_ipython().Completer.all_completions('''%s'''))
+") (python-shell-completion-module-string-code . "';'.join(module_completion('''%s'''))
+") (python-shell-completion-setup-code . "from IPython.core.completerlib import module_completion") (python-shell-interpreter-args . "/home/smallqiang/GIT/runningpad/runningpadweb/manage.py shell") (python-shell-interpreter . "python"))))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
