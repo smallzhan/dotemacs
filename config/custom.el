8bce1bb85b79691afb1b4ea8e4603a9a308b977c
Guoqiang Jin <jguqoiang@foxmail.com>
2015-12-15 13:38:26 +0800

    添加 deft 作为 org-mode 的补充。去掉 flcheck 的 grizzl 补全，采用默认的 helm

--- config/custom.el
+++ config/custom.el
@@ -1,18 +1,44 @@
 ;;=========================================================
 ;;============关于 custom 的配置=============================
 ;;=========================================================
-;;Time-stamp: <smallqiang 2015-09-11 11:23:59>
+;;Time-stamp: <guoqiang 2015-12-08 11:39:31>
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
  '(custom-enabled-themes (quote (tangotango)))
  '(custom-safe-themes
    (quote
-	("41ff656ce344a7d09904ad4c6dff8074b5177d5c3d5d46f64fea5cb29623534b" "d6bb035c861c43421da24bb69d06a65c945c1ee2f38e7e9749fcfd10e6627690" default)))
- '(org-export-backends (quote (ascii beamer html icalendar latex md gfm))))
+	("0aa12caf6127772c1a38f7966de8258e7a0651fb6f7220d0bbb3a0232fba967f" "870a63a25a2756074e53e5ee28f3f890332ddc21f9e87d583c5387285e882099" "41ff656ce344a7d09904ad4c6dff8074b5177d5c3d5d46f64fea5cb29623534b" "d6bb035c861c43421da24bb69d06a65c945c1ee2f38e7e9749fcfd10e6627690" default)))
+ '(fci-rule-color "#37474f")
+ '(hl-sexp-background-color "#1c1f26")
+ '(org-export-backends (quote (ascii beamer html icalendar latex md gfm)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+	((20 . "#f36c60")
+	 (40 . "#ff9800")
+	 (60 . "#fff59d")
+	 (80 . "#8bc34a")
+	 (100 . "#81d4fa")
+	 (120 . "#4dd0e1")
+	 (140 . "#b39ddb")
+	 (160 . "#f36c60")
+	 (180 . "#ff9800")
+	 (200 . "#fff59d")
+	 (220 . "#8bc34a")
+	 (240 . "#81d4fa")
+	 (260 . "#4dd0e1")
+	 (280 . "#b39ddb")
+	 (300 . "#f36c60")
+	 (320 . "#ff9800")
+	 (340 . "#fff59d")
+	 (360 . "#8bc34a"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
